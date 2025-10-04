a3383710bf26a8c3e5e877c76c1e6c77363a176a
KasperSaint07 <123306320+KasperSaint07@users.noreply.github.com>
2025-10-04 21:23:51 +0500

    feat(optimization): add early exit check for sorted input in insertion sort

--- src/main/java/algorithms/InsertionSort.java
+++ src/main/java/algorithms/InsertionSort.java
@@ -1,38 +1,44 @@
-    package algorithms;
+package algorithms;
 
-    import metrics.PerformanceTracker;
+import metrics.PerformanceTracker;
 
-    public class InsertionSort {
-        public static void sort(int[] arr, PerformanceTracker tracker) {
-            if (arr == null || arr.length <= 1) return;
+public class InsertionSort {
 
-            int n = arr.length;
+    public static void sort(int[] arr, PerformanceTracker tracker) {
+        if (arr == null || arr.length <= 1) return;
 
-            for (int i = 1; i < n; i++) {
-                int key = arr[i];
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            boolean isSorted = true;
+
+            int key = arr[i];
+            tracker.incArrayAccesses(1);
+
+            int j = i - 1;
+
+            tracker.incComparisons();
+            if (arr[j] <= key) {
                 tracker.incArrayAccesses(1);
-
-                int j = i - 1;
-
-                // Оптимизация: если текущий элемент уже на месте — пропускаем
-                tracker.incComparisons();
-                if (arr[j] <= key) {
-                    tracker.incArrayAccesses(1);
-                    continue;
-                }
-
-                while (j >= 0 && arr[j] > key) {
-                    tracker.incComparisons();
-                    tracker.incArrayAccesses(1);
-
-                    arr[j + 1] = arr[j];
-                    tracker.incArrayAccesses(1);
-                    tracker.incSwaps();
-                    j--;
-                }
-
-                arr[j + 1] = key;
-                tracker.incArrayAccesses(1);
+                continue;
             }
+
+            while (j >= 0 && arr[j] > key) {
+                tracker.incComparisons();
+                tracker.incArrayAccesses(1);
+
+                arr[j + 1] = arr[j];
+                tracker.incArrayAccesses(1);
+                tracker.incSwaps();
+                j--;
+                isSorted = false;
+            }
+
+            arr[j + 1] = key;
+            tracker.incArrayAccesses(1);
+
+
+            if (isSorted && i == n - 1) break;
         }
     }
+}
